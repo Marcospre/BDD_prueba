c1af508cb0e64230cbda9f5b583543921350c7e5
unknown <2daw3@HZ114478.H012108.NET>
2022-10-11 11:46:06 +0200

    terminado

--- Ejemplo_profe/login.php
+++ Ejemplo_profe/login.php
@@ -3,13 +3,32 @@
 $user = $_GET['user'];
 $pass = $_GET['pass'];
 
+$existe = false;
+
 $mysqli = new mysqli("localhost", "root", "", "test");
 
-if($user=="plaiaundi" && $pass=="1234"){
-    echo "user y pass correctos";
+if ($mysqli->connect_errno) {
+    echo "Falló la conexión a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+$resultado = $mysqli->query("SELECT * FROM login");
+
+while($row = $resultado->fetch_assoc()){
+    if($user==$row['usuario'] && $pass==$row['contrasenia']){
+        $existe = true;
+        break;
+    }
+}
+
+if($existe){
+     echo "user y pass correctos";
     session_start();
     $_SESSION['user']=$user;
     header("Location: dashboard.php");
 }else{
     header("Location: loginform.php");
 }
+
+$mysqli->close();
+
+?>
